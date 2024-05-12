818f51f9ddb2db64be519fca372f131d28597ba8
Alexander Tibanta Miranda <119013329+Alexander-Tibanta23@users.noreply.github.com>
2024-05-12 15:47:23 -0500

    Adding calculator execute class Adding calculator java archiver JAR.

---
+++ src/main/java/ec/edu/epn/CalculatorExecute.java
@@ -0,0 +1,15 @@
+package ec.edu.epn;
+
+public class CalculatorExecute {
+    public static void main(String[] args) {
+
+        System.out.println("Calculadora programa");
+
+        Calculator calc = new Calculator();
+        int sumar = calc.sumar(10, 20);
+        System.out.println("calc.sumar(10,20)" + sumar);
+
+        int restar = calc.restar(40, 20);
+        System.out.println("calc.restar(40,20)" + restar);
+    }
+}
